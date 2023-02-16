3dfea9d5613c0f0dbac670c6ea89373e836ff272
GeniaPiano <genia.piano@hotmail.com>
2023-02-16 19:13:04 +0100

    create basic components

---
+++ src/data/users.js
@@ -0,0 +1,27 @@
+export const users = [
+  {
+    name: 'Adam Romański',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Jakub Sztaba',
+    attendance: '29%',
+    average: '4.1',
+  },
+  {
+    name: 'Antoni Bujalski',
+    attendance: '35%',
+    average: '2.3',
+  },  {
+    name: 'Bartosz Długi',
+    attendance: '35%',
+    average: '2.3',
+  },
+  {
+    name: 'Tadeusz Lipiński',
+    average: '2.3',
+  },
+]
+
+
